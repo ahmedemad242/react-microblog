447189c8532a47d1e061e6fe4ae383551069e19f
Ahmed Emad <50369848+ahmedemad242@users.noreply.github.com>
2023-04-25 11:37:11 +0200

    feat: retrieve posts from backend

---
+++ src/components/Post.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Image from 'react-bootstrap/Image';
+import Stack from 'react-bootstrap/Stack';
+import { Link } from 'react-router-dom';
+
+export default function Post({ post }) {
+  return (
+    <Stack direction="horizontal" gap={3} className="Post">
+      <Image src={post.author.avatar_url + '&s=48'} alt={post.author.username} roundedCircle />
+      <div>
+        <p>
+          <Link to={'/user/' + post.author.username}>{post.author.username}</Link>
+          &nbsp;&mdash;&nbsp;
+          {post.timestamp}
+        </p>
+        <p>{post.text}</p>
+      </div>
+    </Stack>
+  );
+}
